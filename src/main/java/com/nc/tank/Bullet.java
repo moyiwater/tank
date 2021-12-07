361938d4f4d85816b49b845955902a1d1294f629
moyiwater <nwc666666@163.com>
2021-12-07 15:28:12 +0800

    增加子弹类

--- src/main/java/com/nc/tank/Bullet.java
+++ src/main/java/com/nc/tank/Bullet.java
@@ -5,25 +5,31 @@ import lombok.Data;
 import java.awt.*;
 
 @Data
-public class Tank {
-    private int x, y;
-    private Dir dir = Dir.DOWN;
-    private static final int SPEED = 5;
-    private boolean moving = false;
+public class Bullet {
 
-    public Tank(int x, int y, Dir dir) {
+    private static final int SPEED = 1;
+
+    private static int WIDTH = 30, HEIGHT = 30;
+
+    private int x, y;
+
+    private Dir dir;
+
+    public Bullet(int x, int y, Dir dir) {
         this.x = x;
         this.y = y;
         this.dir = dir;
     }
 
     public void paint (Graphics g) {
-        g.fillRect(x, y, 50, 50);
-        move();
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x, y, WIDTH, HEIGHT);
+        g.setColor(c);
+        move ();
     }
 
     private void move () {
-        if (!moving) return;
         switch (dir) {
             case LEFT:
                 x -= SPEED;
